c55812a98b4f157fa61ecf5484542c1350bdc9be
Ibuki Sasao <231091@st.yoshida-g.ac.jp>
2025-10-15 11:12:02 +0900

    終了に入れ忘れ

--- GraduationProject/src/energy_charge.cpp
+++ GraduationProject/src/energy_charge.cpp
@@ -41,9 +41,6 @@ My::CEnergy_Charge::~CEnergy_Charge()
 //======================================================================
 HRESULT My::CEnergy_Charge::Init()
 {
-	m_fTimeSpend = FLOAT_ZERO;
-	m_fChargeSpeed = SPEED_DEF;
-	m_fChargeTime = CHARGE_DEF;
 	return S_OK;
 }
 //======================================================================
@@ -51,6 +48,9 @@ HRESULT My::CEnergy_Charge::Init()
 //======================================================================
 void My::CEnergy_Charge::Uninit()
 {
+	m_fTimeSpend = FLOAT_ZERO;
+	m_fChargeSpeed = SPEED_DEF;
+	m_fChargeTime = CHARGE_DEF;
 }
 
 //======================================================================
