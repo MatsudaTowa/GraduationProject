9406520f2681f0ef84390d8c022957a59b33d74c
souma umeda <231074@st.yoshida-g.ac.jp>
2025-10-31 16:25:53 +0900

    クラス追加

--- GraduationProject/src/target_arrow.h
+++ GraduationProject/src/target_arrow.h
@@ -1,1 +1,27 @@
-#pragma once
+//=============================================
+//
+// ターゲットアロー[target_arrow.h]
+// Author : 梅田爽真
+//
+//=============================================
+#ifndef _TARGET_ARROW_ //これが定義されてないとき
+#define _TARGET_ARROW_
+#include "main.h"
+
+/** @brief My 名前空間 */
+namespace My
+{
+	/** @brief テクスチャ管理クラス */
+	class CTargetArrow:public CObject2D
+	{
+	public:
+		CTargetArrow();
+		~CTargetArrow();
+		HRESULT Init()override;
+
+	private:
+		
+	};
+}
+
+#endif
