7a9f6727e08c1dc524f1cb2ed45e3f397ef5ad69
Matsuda Towa <231084@st.yoshida-g.ac.jp>
2025-11-09 01:10:00 +0900

    duel_managerを追加。ここにデュエル中に使うカードがすべて格納されます

---
+++ GraduationProject/src/duel_manager.cpp
@@ -0,0 +1,61 @@
+//================================
+//
+//duel_manager.cppに必要な宣言[duel_manager.cpp]
+//Author:松田永久
+// 
+//================================
+#include "duel_manager.h"
+
+//================================
+// コンストラクタ
+//================================
+My::CDuelManager::CDuelManager()
+{
+	m_UseCardList.clear();
+}
+
+//================================
+// デストラクタ
+//================================
+My::CDuelManager::~CDuelManager()
+{
+	m_UseCardList.clear();
+}
+
+//================================
+// 初期化
+//================================
+HRESULT My::CDuelManager::Init()
+{
+	return S_OK;
+}
+
+//================================
+// 終了
+//================================
+void My::CDuelManager::Uninit()
+{
+}
+
+//================================
+// カードのリストに登録
+//================================
+void My::CDuelManager::RegistUseCardList(CCard* card)
+{
+	//ターゲットリストの情報を登録
+	m_UseCardList.push_back(card);
+}
+
+//================================
+// カードのリストから削除
+//================================
+void My::CDuelManager::RemoveUseCardList(CCard* card)
+{
+	//サイズが0なら抜ける
+	if (m_UseCardList.size() == 0)
+	{
+		return;
+	}
+	//カードの情報を削除
+	m_UseCardList.remove(card);
+}
--- GraduationProject/src/duel_manager.h
+++ GraduationProject/src/duel_manager.h
@@ -1,103 +1,50 @@
-////================================
-////
-////duel_manager.cppに必要な宣言[duel_manager.h]
-////Author:松田永久
-//// 
-////================================
+//================================
 //
-//#ifndef _DUEL_MANAGER_H_ //このマクロ定義がされていなかったら
-//#define _DUEL_MANAGER_H_ //２重インクルード防止のマクロ定義
-//
-//namespace My
-//{
-//	class CActiveScenePlayer;
-//	class CCard;
-//	//デュエルマネージャーのクラス
-//	class CDuelManager
-//	{
-//	public:
-//
-//		//メンバ関数
-//		CDuelManager();			//コンストラクタ
-//		~CDuelManager();			//デストラクタ
-//		HRESULT Init();				//初期化
-//
-//		/**
-//		 * @brief 終了
-//		 */
-//		void Uninit();
-//
-//		/**
-//		 * @brief 自分自身のインスタンスを生成し取得
-//		 */
-//		static CDuelManager* GetInstance()
-//		{
-//			//静的インスタンス
-//			static CDuelManager instance;
-//			return &instance;
-//		}
-//		/**
-//		 * @brief エリアを生成
-//		 */
-//		void CreateArea();
-//
-//		/**
-//		 * @brief エリアの選択
-//		 */
-//		void SelectArea();
-//
-//		/**
-//		 * @brief そこのエリアにカードを実行
-//		 * @param [in]area
-//		 */
-//		void CardTrigger(My::CInputMouse::AREA area);
-//
-//		/**
-//		 * @brief エリアの設定
-//		 * @param [in]エリアのポインタ
-//		 * @param [in]どこのエリアか
-//		 */
-//		void SetArea(CArea* area, int idx) { m_pArea[idx] = area; }
-//
-//		/**
-//		 * @brief 中心エリアの設定
-//		 * @param 中心エリア
-//		 */
-//		void SetCenterArea(CCenterArea* area) { m_pCenterArea = area; }
-//
-//		/**
-//		 * @brief エリアの取得
-//		 * @param [in]エリア番号
-//		 * @return エリアのポインタ
-//		 */
-//		CArea* GetArea(int idx) { return m_pArea[idx]; }
-//
-//		/**
-//		 * @brief 中心エリアの取得
-//		 * @return 中心エリア
-//		 */
-//		CCenterArea* GetCenterArea() { return m_pCenterArea; }
-//
-//		/**
-//		 * @brief 選択しているか取得
-//		 * @return
-//		 */
-//		inline bool GetActive()
-//		{
-//			return m_isActive;
-//		}
-//
-//		/**
-//		 * @brief 選択されているか設定
-//		 * @param [in]isSelect
-//		 */
-//		inline void SetActive(bool isActive)
-//		{
-//			m_isActive = isActive;
-//		}
-//
-//	private:
-//	};
-//}
-//
-//#endif
+//duel_manager.cppに必要な宣言[duel_manager.h]
+//Author:松田永久
+// 
+//================================
+
+#ifndef _DUEL_MANAGER_H_ //このマクロ定義がされていなかったら
+#define _DUEL_MANAGER_H_ //２重インクルード防止のマクロ定義
+
+namespace My
+{
+	class CActiveScenePlayer;
+	class CCard;
+	//デュエルマネージャーのクラス
+	class CDuelManager
+	{
+	public:
+
+		//メンバ関数
+		CDuelManager();			//コンストラクタ
+		~CDuelManager();			//デストラクタ
+		HRESULT Init();				//初期化
+
+		/**
+		 * @brief 終了
+		 */
+		void Uninit();
+
+		/**
+		 * @brief 自分自身のインスタンスを生成し取得
+		 */
+		static CDuelManager* GetInstance()
+		{
+			//静的インスタンス
+			static CDuelManager instance;
+			return &instance;
+		}
+
+		void RegistUseCardList(CCard* card);	//登録
+		void RemoveUseCardList(CCard* card);	//削除
+
+		//リストの取得
+		inline std::list<CCard*> GetUseCardList() { return m_UseCardList; }
+	private:
+		std::list<CCard*> m_UseCardList; //このゲームで使うカードのリスト
+	};
+}
+
+#endif
