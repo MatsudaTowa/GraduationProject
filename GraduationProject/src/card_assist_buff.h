5a8d3a2972d2d5a85b912d3ef01eb8abe1cb9456
Matsuda Towa <231084@st.yoshida-g.ac.jp>
2025-11-10 14:17:49 +0900

    カードを細かくクラス分け

---
+++ GraduationProject/src/card_assist_buff.h
@@ -0,0 +1,64 @@
+//===========================================================================================================================================================
+// 
+// アシスト(バフ)カード [card_assist_buff.h]
+// Author : 松田 永久
+// 
+//===========================================================================================================================================================
+#ifndef _CARD_AST_BUFF_H_
+#define _CARD_AST_BUFF_H_
+#include "main.h"
+#include "card_assist.h"
+
+namespace My
+{
+	class CCardAssist_Buff :public CCardAssist
+	{
+	public:
+
+		enum BuffType
+		{// バフの種類 TODO:継承によって変わるかも
+			NONE_BUFF,
+			HEAL,                // HP回復
+			NO_NAMEONE,            // 未定①
+			NO_NAMETWO,            // 未定②
+		};
+
+		/**
+		 * @brief コンストラクタ
+		 */
+		CCardAssist_Buff(int nPriority);
+
+		/**
+		 * @brief デストラクタ
+		 */
+		~CCardAssist_Buff()override;
+
+		/**
+		 * @brief 初期化
+		 */
+		HRESULT Init()override;
+
+		/**
+		 * @brief 終了
+		 */
+		void Uninit()override;
+
+		/**
+		 * @brief 更新
+		 */
+		void Update()override;
+
+		/**
+		 * @brief 描画
+		 */
+		void Draw()override;
+
+		inline BuffType GetBuffType() { return m_BuffType; }
+
+		inline void SetBuffType(BuffType buff_type) { m_BuffType = buff_type; }
+	private:
+		BuffType m_BuffType;
+	};
+}
+
+#endif // !_CARD_AST_H_
