65308403d380bdc7bfcd20a17a295c41a05fcb14
souma umeda <231074@st.yoshida-g.ac.jp>
2025-11-17 10:51:25 +0900

    target_arrow_managerのファイルを追加

---
+++ GraduationProject/src/target_arrow_manager.h
@@ -0,0 +1,57 @@
+//===========================================================================================================================================================
+// 
+// target_arrow_manager.cppのヘッダー [target_arrow_manager.h]
+// Author : souma umeda
+// 
+//===========================================================================================================================================================
+#ifndef _TARGET_ARROW_MANAGER_H_
+#define _TARGET_ARROW_MANAGER_H_
+#include "main.h"
+
+namespace My
+{
+	class CTargetArrow;
+
+	class CTargetArrowManager
+	{
+	public:
+		/**
+		* @brief コンストラクタ
+		*/
+		CTargetArrowManager();
+
+		/**
+		* @brief デストラクタ
+		*/
+		~CTargetArrowManager();
+
+		/**
+		* @brief 登録
+		* @param [in]ターゲットアローのポインタ
+		*/
+		void Regist(CTargetArrow*pTA);
+
+		/**
+		* @brief 登録
+		* @param [in]ターゲットアローのポインタ
+		*/
+		void Delete();
+
+		/**
+		* @brief ターゲットアローリストの取得処理
+		* @param [out]ターゲットアローリストのポインタ
+		*/
+		std::list<CTargetArrow*> GetTargetArrow();
+
+	private:
+		/**
+		* @brief ターゲットアローのリスト
+		*/
+		std::list<CTargetArrow*>m_pTargetArrowList;
+
+	};
+}
+
+
+
+#endif // !TARGET_ARROW_MANAGER_H_
