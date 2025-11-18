4b6669cb3fb6bbf9a3948f9387a0584e41261834
KasaiKeisuke <231020@st.yoshida-g.ac.jp>
2025-11-18 09:44:04 +0900

    ファイルの追加やり直し

---
+++ GraduationProject/src/cpp_restapi/include/cpp_restapi/iconnection.hpp
@@ -0,0 +1,31 @@
+
+#ifndef ACONNECTION_HPP
+#define ACONNECTION_HPP
+
+#include <string>
+
+namespace cpp_restapi
+{
+    /**
+     * @brief Interface representing connection with rest api server
+     */
+    struct IConnection
+    {
+        public:
+            virtual ~IConnection() = default;
+
+            /**
+             * @brief perform a request to api
+             * @param request api request. For example "users/SomeUserName/repos"
+             * @return api response in json format
+             */
+            virtual std::string get(const std::string& request) = 0;
+
+            /**
+             * @brief return API url
+             */
+            virtual const std::string& url() const = 0;
+    };
+}
+
+#endif
---
+++ GraduationProject/src/cpp_restapi/src/base_connection.cpp
@@ -0,0 +1,87 @@
+
+#include <json/value.h>
+#include <json/reader.h>
+#include <json/writer.h>
+
+#include <cpp_restapi/base_connection.hpp>
+#include "header_utils.hpp"
+
+
+namespace
+{
+    void update(Json::Value& a, const Json::Value& b)
+    {
+        if (a.isArray() && b.isArray())
+            for (const auto& entry : b)
+                a.append(entry);
+        else if (a.isObject() && b.isObject())
+        {
+            for (const auto& key : b.getMemberNames())
+                if (a[key].isObject())
+                    update(a[key], b[key]);
+                else
+                    a[key] = b[key];
+        }
+    }
+}
+
+namespace cpp_restapi
+{
+
+BaseConnection::BaseConnection(const std::string& address, const std::map<std::string, std::string>& headerEntries)
+    : m_address(address)
+    , m_headerEntries(headerEntries)
+{
+
+}
+
+
+std::string BaseConnection::get(const std::string& request)
+{
+    std::string nextPage = m_address + "/" + request;
+
+    Json::Value output;
+
+    do
+    {
+        Json::Reader reader;
+
+        const std::pair<std::string, std::string> response = fetchPage(nextPage);
+        const std::string& header_links = response.second;
+
+        Json::Value partialOutput;
+        reader.parse(response.first, partialOutput);
+
+        if (output.isNull())
+            output.swap(partialOutput);
+        else
+            update(output, partialOutput);
+
+        nextPage = HeaderUtils::getNextPageUrl(header_links);
+    }
+    while (nextPage.empty() == false);
+
+    Json::FastWriter fastWriter;
+    const std::string strOutput = fastWriter.write(output);
+
+    return strOutput;
+}
+
+
+const std::string& BaseConnection::url() const
+{
+    return m_address;
+}
+
+const std::map<std::string, std::string>& BaseConnection::getHeaderEntries() const
+{
+    return m_headerEntries;
+}
+
+
+const std::string & BaseConnection::address() const
+{
+    return m_address;
+}
+
+}
