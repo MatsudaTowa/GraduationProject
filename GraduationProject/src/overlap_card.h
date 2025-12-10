5baff9ade8869e02b81fe19e138c094e71587f18
souma umeda <231074@st.yoshida-g.ac.jp>
2025-12-10 11:53:37 +0900

    overlap_card.cpp,h追加

---
+++ GraduationProject/src/overlap_card.h
@@ -0,0 +1,74 @@
+//============================================================================================
+// 
+// overlap_card.cpp のヘッダー [overlap_card.h]
+// Author : Umeda Souma
+// 
+//============================================================================================
+#ifndef _OVERLAP_CARD_
+#include "main.h"
+#include "card.h"
+
+/** @brief My 名前空間 */
+namespace My
+{
+	class COverlapCard
+	{
+	public:
+		/**
+		* @brief コンストラクタ
+		*/
+		COverlapCard();
+
+		/**
+		* @brief デストラクタ
+		*/
+		virtual ~COverlapCard();
+
+		/**
+		* @brief 登録
+		* @param [in]pCard カードのポインタ
+		*/
+		inline void Regist(CCard* pCard) { m_pOverlapCards.push_back(pCard); }
+
+		/**
+		* @brief 削除
+		*/
+		void ReMove(CCard* pCard);
+
+		/**
+		* @brief クリア
+		*/
+		inline void Clear() { m_pOverlapCards.clear(); }
+
+		/**
+		* @biref 重複しているカードのポインタ配列を取得
+		* @return 重複しているカードのポインタ配列
+		*/
+		inline std::vector<CCard*> GetOverlapCards() { return m_pOverlapCards; }
+	private:
+		/**
+		* @brief 重複しているカードのポインタ配列
+		*/
+		std::vector<CCard*> m_pOverlapCards;
+	};
+
+	//====================================================================
+	// 重なっている攻撃カード
+	//====================================================================
+	class COverlapAttackCard : public COverlapCard
+	{
+	public:
+		/**
+		* @brief コンストラクタ
+		*/
+		COverlapAttackCard() {};
+
+		/**
+		* @brief デストラクタ
+		*/
+		~COverlapAttackCard()override {};
+	};
+}
+
+
+#endif // !_OVERLAP_CARD_
