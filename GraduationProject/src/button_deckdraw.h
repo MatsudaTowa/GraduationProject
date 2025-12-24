1f9ee94a267293c0e4ac3ba05d0bd19e17e2e2e5
souma umeda <231074@st.yoshida-g.ac.jp>
2025-12-24 14:05:49 +0900

    ボタンを追加するの忘れてました

---
+++ GraduationProject/src/button_deckdraw.h
@@ -0,0 +1,72 @@
+//===============================================================================================================
+// 
+// overlap_card_manager.cpp のヘッダー [overlap_card_manager.h]
+// Author : Umeda Souma
+// 
+//===============================================================================================================
+#ifndef _BUTTON_DECKDRAW_
+#define _BUTTON_DECKDRAW_
+#include "main.h"
+#include "button.h"
+
+
+
+namespace My
+{
+	class CPlayerDuelState;
+
+	class CButtonDeckDraw : public CButton
+	{
+	public:
+		/**
+		 * @brief コンストラクタ
+		 * @param [in]プライオリティ
+		 */
+		CButtonDeckDraw(int nPriority = PRIORITY);
+		/**
+		 * @brief デストラクタ
+		 */
+		~CButtonDeckDraw()override;
+		/**
+		 * @brief 初期化
+		 * @return 成功したか
+		 */
+		HRESULT Init()override;
+		/**
+		 * @brief 終了
+		 */
+		void Uninit()override;
+		/**
+		 * @brief 更新
+		 */
+		void Update()override;
+		/**
+		 * @brief 描画
+		 */
+		void Draw()override;
+
+		/**
+		 * @brief マウスの判定
+		 * @return 当たっているか
+		 */
+		bool ProcessMouseEvent(CPlayerDuelState* state);
+
+		/**
+		 * @brief ボタンが押された時の処理
+		 */
+		void ButtonTrigger()override;
+
+		/**
+		 * @brief 生成
+		 * @param [in]位置
+		 * @param [in]サイズ
+		 * @return
+		 */
+		static CButtonDeckDraw* Create(D3DXVECTOR3 pos, D3DXVECTOR2 size);
+	private:
+
+	};
+}
+
+
+#endif //_OVERLAP_CARD_MANAGER_
